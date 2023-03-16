d1973eed7c2fde59ffbd8207fde2fa587038d334
sanjeevkumar <sanjeev.kumar@rapidfunnel.com>
2023-03-17 02:35:46 +0530

    k

--- src/routes/user.js
+++ src/routes/user.js
@@ -7,15 +7,15 @@ import { userValidationRules, validate } from '../validation/userValidator.js';
 import { isUserExist } from '../middleware/middleware.js';
 
 const router = express.Router();
-// const upload = multer();
+const upload = multer();
 
-// router.post('/registration',upload.none(),isUserExist,userValidationRules(),validate,catchAsync(userRegistration));
-// router.post('/login',upload.none(),isUserExist,userValidationRules(),validate,catchAsync(userLogin));
+router.post('/registration',upload.none(),isUserExist,userValidationRules(),validate,catchAsync(userRegistration));
+router.post('/login',upload.none(),isUserExist,userValidationRules(),validate,catchAsync(userLogin));
 
-// router.use((err, req, res, next) => {
-//    const { message = 'something went wrong', status = 500} = err;
-//    logger.error(message);
-//    res.status(status).send(message)
-// })
+router.use((err, req, res, next) => {
+   const { message = 'something went wrong', status = 500} = err;
+   //logger.error(message);
+   res.status(status).send(message)
+})
 
 export default router;
