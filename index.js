a1b2a6b933795477585a86aedccc36f5c413b3e1
sanjeevkumar <sanjeev.kumar@rapidfunnel.com>
2023-03-17 02:11:51 +0530

    k

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
-import eventRoute from './src/routes/event.js';
-import userRoute from './src/routes/user.js';
+//import eventRoute from './src/routes/event.js';
+//import userRoute from './src/routes/user.js';
 import * as dotenv from 'dotenv';
 import connectDatabase from './src/database/connection.js';
 import morgan from 'morgan';
@@ -22,13 +22,13 @@ app.use(mongoSanitize({replaceWith: '_'}));
 app.use(helmet());
 
 //setting up db connection
-//connectDatabase();
+connectDatabase();
  
 // setup the logger
-app.use(morgan('combined', {stream: fs.createWriteStream(`${path.join(__dirname, 'logs')}/access.log`, {flags: 'a'})}));
+//app.use(morgan('combined', {stream: fs.createWriteStream(`${path.join(__dirname, 'logs')}/access.log`, {flags: 'a'})}));
 
-app.use('/event', eventRoute);
-app.use('/user', userRoute);
+//app.use('/event', eventRoute);
+//app.use('/user', userRoute);
 
 app.use((req,res) => {res.status(404).send('Invalid URL entered!')})
 
